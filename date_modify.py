7058f35eb3ecb7dc751bd3edcc25e2a887556ec0
David Sanchez <david38sanchez@gmail.com>
2020-12-12 22:05:57 -0500

    Adding code + tests to handle more examples

--- date_modify.py
+++ date_modify.py
@@ -11,12 +11,25 @@ Usage:
 from datetime import datetime, timedelta
 from dateutil.relativedelta import *
 import re
+import time
 
-YESTERDAY_REGEX = r'yesterday (([0-9]|0[0-9]|1[0-9]|2[0-3]):([0-5][0-9])|noon)'
-DAYS_REGEX = r'(?i)sunday|monday|tuesday|wednesday|thursday|friday|saturday|sun|mon|tue|wed|thu|fri|sat'
-RELTEXT_SPACE_WEEK_REGEX = r'(?i)(sunday|monday|tuesday|wednesday|thursday|friday|saturday|sun|mon|tue|wed|thu|fri|sat) (last|this|next) week'
+# Values
+WEEKDAYS = 'sunday|monday|tuesday|wednesday|thursday|friday|saturday'
+WEEKDAYS_3 = 'sun|mon|tue|wed|thu|fri|sat'
+UNITS = 'sec|second|min|minute|hour|day|week|month|year'
+TIME_FORMAT = '([0-9]|0[0-9]|1[0-9]|2[0-3]):([0-5][0-9])'
+
+# Regex
+YESTERDAY_REGEX = r'yesterday ({0}|noon)'.format(TIME_FORMAT)
+DAYS_REGEX = r'(?i){0}|{1}'.format(WEEKDAYS, WEEKDAYS_3)
+RELTEXT_SPACE_WEEK_REGEX = r'(?i)({0}|{1}) (last|this|next) week'.format(WEEKDAYS, WEEKDAYS_3)
 NEXT_WEEK_REGEX = r'(?i)^next (\w+)$'
 NEXT_WEEK_MORE_REGEX = r'(?i)^next (\w+) (\+|-)([0-9]+) (\w+)$'
+LAST_WEEK_REGEX = r'(?i)^last (\w+)$'
+LAST_WEEK_MORE_REGEX = r'(?i)^last (\w+) (\+|-)([0-9]+) (\w+)$'
+AGO_SIMPLE = r'(?i)^([0-9]*) (({0})s?) ago$'.format(UNITS)
+AGO_TIME = r'(?i)^([0-9]*) (({0})s?) ago {1}$'.format(UNITS, TIME_FORMAT)
+AGO_COMPLEX = r'(?i)^([0-9]*) (({0})s?)( ago)? ([0-9]*) (({0})s?)( ago)?'.format(UNITS)
 
 
 class DateModify:
@@ -59,27 +72,50 @@ class DateModify:
             return self.increase_decrease_unit(datetime=next_day, operator=regex.group(2), value=regex.group(3),
                                                unit=regex.group(4))
         # Last `dayname`
-        elif re.match(r'^last (\w+)$', modifier):
-            the_day = re.match(r'(?i)^last (.*)$', modifier).group(1)
-            this_day = self.datetime - timedelta(days=1)
-            while not re.match(r"(?i)^" + this_day.strftime("%A") + "$", the_day):
-                this_day -= timedelta(days=1)
-            return this_day
+        elif re.match(LAST_WEEK_REGEX, modifier):
+            the_day = re.match(LAST_WEEK_REGEX, modifier).group(1)
+            return self.get_last_dayname(the_day)
+        # Last `dayname` with increase|decrease of unit time
+        elif re.match(LAST_WEEK_MORE_REGEX, modifier):
+            regex = re.match(LAST_WEEK_MORE_REGEX, modifier)
+            last_day = self.get_last_dayname(regex.group(1))
+            return self.increase_decrease_unit(datetime=last_day, operator=regex.group(2), value=regex.group(3),
+                                               unit=regex.group(4))
         # Handles the special format "weekday + last/this/next week".
         elif re.match(RELTEXT_SPACE_WEEK_REGEX, modifier):
             regex = re.match(r'(?i)^(\w+) (last|this|next) week$', modifier)
-            print(regex.group(1))
-            print(regex.group(2))
-            return ""
+            if re.match(r'(?i)^last$', regex.group(2)):
+                return self.get_last_dayname(regex.group(1))
+            # TODO: Currently not working
+            elif re.match(r'(?i)^this$', regex.group(2)):
+                tm_wday = time.strptime(regex.group(1),
+                                        "%A" if re.match(r"(?i)^{0}$".format(WEEKDAYS), modifier) else "%a").tm_wday
+                return self.datetime - timedelta(days=(7 - tm_wday) % 7)
+            elif re.match(r'(?i)^next$', regex.group(2)):
+                return self.get_next_dayname(regex.group(1))
         # Moves to the next day of this name.
         elif re.match(DAYS_REGEX, modifier):
             this_day = self.datetime + timedelta(days=1)
-            while not re.match(r"(?i)^" + this_day.strftime("%A") + "$", modifier):
-                this_day += timedelta(days=1)
+            if re.match(r"(?i)^{0}$".format(WEEKDAYS), modifier):
+                while not re.match(r"(?i)^{0}$".format(this_day.strftime("%A")), modifier):
+                    this_day += timedelta(days=1)
+            else:
+                while not re.match(r"(?i)^{0}$".format(this_day.strftime("%a")), modifier):
+                    this_day += timedelta(days=1)
             return this_day
-        #
-        elif re.match(r'^ago$', modifier):
-            return ""
+        # Negates all the values of previously found relative time items.
+        elif re.match(r'(?i)(.*)ago(.*)', modifier):
+            if re.match(AGO_SIMPLE, modifier):
+                regex = re.match(AGO_SIMPLE, modifier)
+                return self.increase_decrease_unit(self.datetime, "-", int(regex.group(1)), regex.group(2))
+            elif re.match(AGO_TIME, modifier):
+                regex = re.match(AGO_TIME, modifier)
+                datetime = self.increase_decrease_unit(self.datetime, "-", int(regex.group(1)), regex.group(2))
+                return datetime - timedelta(hours=float(regex.group(4)), minutes=float(regex.group(5)))
+            elif re.match(AGO_COMPLEX, modifier):
+                regex = re.match(AGO_COMPLEX, modifier)
+                datetime = self.increase_decrease_unit(self.datetime, "-", int(regex.group(1)), regex.group(2))
+                return self.increase_decrease_unit(datetime, "-", int(regex.group(5)), regex.group(6))
 
     # Set time to 12:00:00
     @staticmethod
@@ -94,13 +130,20 @@ class DateModify:
     # Return next `dayname` from reltext
     def get_next_dayname(self, the_day):
         this_day = self.datetime + timedelta(days=1)
-        while not re.match(r"(?i)^" + this_day.strftime("%A") + "$", the_day):
+        while not re.match(r"(?i)^{0}$".format(this_day.strftime("%A")), the_day):
             this_day += timedelta(days=1)
+        return this_day
+
+    # Return last `dayname` from reltext
+    def get_last_dayname(self, the_day):
+        this_day = self.datetime + timedelta(days=1)
+        while not re.match(r"(?i)^{0}$".format(this_day.strftime("%A")), the_day):
+            this_day -= timedelta(days=1)
         return this_day
 
     @staticmethod
     def increase_decrease_unit(datetime, operator, value, unit):
-        if re.match(r'^((sec|second|min|minute|hour|day|fortnight|forthnight|month|year)s?)|weeks$', unit) is None:
+        if re.match(r"^(({0})s?)|weeks$".format(UNITS), unit) is None:
             raise Exception("Not supported unit")
 
         if re.match(r'^(sec|second)s?$', unit):
@@ -122,5 +165,4 @@ class DateModify:
             return datetime + delta
         elif operator == "-":
             return datetime - delta
-        else:
-            return datetime
+        return datetime
--- main.py
+++ main.py
@@ -39,8 +39,38 @@ class TestDateModify(unittest.TestCase):
     def test_last_dayname(self):
         self.assertEqual(self.dm.modify("last friday").isoformat(), '2019-12-27T00:00:00')
 
+    def test_last_dayname_more(self):
+        self.assertEqual(self.dm.modify("last saturday -2 hours").isoformat(), '2019-12-27T22:00:00')
+
     def test_weekday(self):
+        self.assertEqual(self.dm.modify("sunday").isoformat(), '2020-01-05T00:00:00')
+        self.assertEqual(self.dm.modify("sun").isoformat(), '2020-01-05T00:00:00')
         self.assertEqual(self.dm.modify("Monday").isoformat(), '2020-01-06T00:00:00')
+        self.assertEqual(self.dm.modify("mon").isoformat(), '2020-01-06T00:00:00')
+        self.assertEqual(self.dm.modify("tuesday").isoformat(), '2020-01-07T00:00:00')
+        self.assertEqual(self.dm.modify("tue").isoformat(), '2020-01-07T00:00:00')
+        self.assertEqual(self.dm.modify("wednesday").isoformat(), '2020-01-08T00:00:00')
+        self.assertEqual(self.dm.modify("wed").isoformat(), '2020-01-08T00:00:00')
+        self.assertEqual(self.dm.modify("thursday").isoformat(), '2020-01-02T00:00:00')
+        self.assertEqual(self.dm.modify("thu").isoformat(), '2020-01-02T00:00:00')
+        self.assertEqual(self.dm.modify("friday").isoformat(), '2020-01-03T00:00:00')
+        self.assertEqual(self.dm.modify("fri").isoformat(), '2020-01-03T00:00:00')
+        self.assertEqual(self.dm.modify("saturday").isoformat(), '2020-01-04T00:00:00')
+        self.assertEqual(self.dm.modify("sat").isoformat(), '2020-01-04T00:00:00')
+
+    def test_ago(self):
+        self.assertEqual(self.dm.modify("2 days ago").isoformat(), '2019-12-30T00:00:00')
+        self.assertEqual(self.dm.modify("8 days ago 14:00").isoformat(), '2019-12-23T10:00:00')
+        self.assertEqual(self.dm.modify("2 months 5 days ago").isoformat(), '2019-10-27T00:00:00')
+        self.assertEqual(self.dm.modify("2 months ago 5 days").isoformat(), '2019-10-27T00:00:00')
+
+    def test_reltext_week(self):
+        self.assertEqual(self.dm.modify("Tuesday last week").isoformat(), '2019-12-31T00:00:00')
+        self.assertEqual(self.dm.modify("Friday last week").isoformat(), '2019-12-27T00:00:00')
+        self.assertEqual(self.dm.modify("Monday next week").isoformat(), '2020-01-06T00:00:00')
+        self.assertEqual(self.dm.modify("wednesday next week").isoformat(), '2020-01-08T00:00:00')
+        # self.assertEqual(self.dm.modify("wednesday this week").isoformat(), '2020-01-01T00:00:00')
+        # self.assertEqual(self.dm.modify("fri this week").isoformat(), '2020-01-03T00:00:00')
 
 
 if __name__ == '__main__':
